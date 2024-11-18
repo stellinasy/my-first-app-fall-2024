acb8db1e058e3a294efb9dde2f22695bf9200f87
stellinasy <scs173@georgetown.edu>
2024-11-18 17:49:55 -0500

    (ignore)

--- README.md
+++ README.md
@@ -53,13 +53,14 @@ Run the unemployment report:
 ```sh
 #ALPHAVANTAGE_API_KEY="..." python app/unemployment.py
 
-python app/unemployment.py
+python -m app.unemployment
 ```
 
 Run the stocks report:
 
 ```sh
-python app/stocks.py
+python -m app.stocks
+
 ```
 
 Run the example email sending file:
--- app/stocks.py
+++ app/stocks.py
@@ -9,9 +9,9 @@ API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
 
 def fetch_stocks_csv(symbol):
-     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
-     df = read_csv(request_url)
-     return df
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
+    df = read_csv(request_url)
+    return df
 
 
 if __name__ == "__main__":
