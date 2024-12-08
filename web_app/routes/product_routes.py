5f9288e32d0dc569b432085ef4db5c9987004ade
stellinasy <scs173@georgetown.edu>
2024-12-07 20:52:59 -0500

    Creating the products page

---
+++ web_app/routes/product_routes.py
@@ -0,0 +1,32 @@
+# this is the "web_app/routes/product_routes.py" file...
+
+from flask import Blueprint, request, render_template
+
+product_routes = Blueprint("product_routes", __name__)
+
+@product_routes.route("/products")
+def index():
+    print("PRODUCTS...")
+    
+    #this is some data for the internet
+    products = [
+        {
+            'name': 'Strawberries',
+            'description': 'Juicy organic strawberries.',
+            'price': 4.99,
+            'url': 'https://picsum.photos/id/1080/360/200'
+        },
+        {
+            'name': 'Cup of Tea',
+            'description': 'An individually-prepared tea or coffee of choice.',
+            'price': 3.49,
+            'url': 'https://picsum.photos/id/225/360/200'
+        },
+        {
+            'name': 'Textbook',
+            'description': 'It has all the answers.',
+            'price': 129.99,
+            'url': 'https://picsum.photos/id/24/360/200'
+        }
+    ]
+    return render_template("products.html", products=products)
